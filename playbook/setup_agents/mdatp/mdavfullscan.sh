b2b153e90da43f2e04a814820f2666c7ca1f974c
yucelsan <contact@yucelsan.fr>
2025-02-12 00:45:48 +0100

    Playbooks Ansible déploiement Agents
    Installation et configuration des agents mdatp, site24x7, Tanium et Nutanix Guest Tools, OMS AMA AZURE ARC...

---
+++ playbook/setup_agents/mdatp/mdavfullscan.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Génère un délai aléatoire entre 0 et 1200 secondes (20 minutes)
+DELAY=$((RANDOM % 1200))
+
+# Affiche le délai pour des raisons de débogage (facultatif)
+echo "Waiting for $DELAY seconds before executing the task"
+
+# Attend pendant le délai aléatoire
+sleep $DELAY
+
+set -e
+echo $(date) "Time Scan Begins" >>/var/log/mdav_crontab_full_scan.log
+/bin/mdatp scan full >> /var/log/mdav_crontab_full_scan.log
+echo $(date) "Time Scan Finished" >>/var/log/mdav_crontab_full_scan.log
+exit 0
